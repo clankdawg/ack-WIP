d5e3e7edb9a99f272a3180dbcdb17ad79dc6dbcc
clankdawg <adam.jason.clark@outlook.com>
2025-07-14 18:44:18 +0100

    Added requirements, updated Makefile, updated linker, compiling using cross compiler now, update stdint.h to fix compiler error, update README.md

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ack
+Ack is an OS project that i made as a hobby OS. It is not intended to be a daily driver, just a cool little project i made.
--- src/stdint.h
+++ src/stdint.h
@@ -1,15 +1,17 @@
 typedef signed char int8_t;
 typedef unsigned char uint8_t;
-
 typedef signed short int16_t;
 typedef unsigned short uint16_t;
-
 typedef signed long int int32_t;
 typedef unsigned long int uint32_t;
-
 typedef signed long long int int64_t;
 typedef unsigned long long int uint64_t;
 
+#ifndef __cplusplus
+#if !defined(__STDC_VERSION__) || __STDC_VERSION__ < 199901L
 typedef uint8_t bool;
+#endif
+#endif
+
 #define true 1
 #define false 0
